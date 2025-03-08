05d9891666883d492c9fc2b208baaaba89e285e2
mrunali2311 <lily.devil1125@gmail.com>
2025-03-08 19:32:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
- import express from "express";
+import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+
+
+
+
 dotenv.config();
 
-const authRoutes = require("./routes/authRoutes"); // FIXED
+import authRoutes from "./routes/authRoutes.js";
+
 
 const app = express();
 app.use(express.json());
