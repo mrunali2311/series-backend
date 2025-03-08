6302eb6501c14c645e533b7d7f19a4ed389e9ac0
mrunali2311 <lily.devil1125@gmail.com>
2025-03-08 18:07:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+
+
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Series Web App Backend!");
+  res.send("Welcome to Series Web App Backend");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
