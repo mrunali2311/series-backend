095966350df3bb7c06984b3f212c035591dbe867
mrunali2311 <lily.devil1125@gmail.com>
2025-03-08 19:52:47 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Friends List
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   location: { type: String, default: "" } // Store last known location
 });
 
-module.exports = mongoose.model("User", UserSchema);
+export default mongoose.model("User", UserSchema);
