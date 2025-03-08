c1098f74cb3b64575b039a77efd84be09cc6bfb1
mrunali2311 <lily.devil1125@gmail.com>
2025-03-08 19:13:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
-
-const express = require("express");
+  const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes"); // FIXED
+
 const app = express();
 app.use(express.json());
 app.use(cors());
