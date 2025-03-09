ee60c85424df5b751a17a8ae3ce9b7b494139839
mrunali2311 <lily.devil1125@gmail.com>
2025-03-09 15:03:54 +0530

    Update seriesRoutes.js

--- routes/seriesRoutes.js
+++ routes/seriesRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { getAllSeries, createSeries } from "../controllers/seriesController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
+
+router.get("/", getAllSeries);
+router.post("/", authMiddleware, createSeries);
+
+export default router;
