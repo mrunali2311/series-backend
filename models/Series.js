043a1e1fc1a689dcbe028a1e53d7957c4f8b7755
mrunali2311 <lily.devil1125@gmail.com>
2025-03-09 15:02:47 +0530

    Update Series.js

--- models/Series.js
+++ models/Series.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+const seriesSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    genre: { type: String, required: true },
+    releaseDate: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Series", seriesSchema);
