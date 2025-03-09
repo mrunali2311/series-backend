708564957bb2745a243b3464c77b77d3e5f1ff55
mrunali2311 <lily.devil1125@gmail.com>
2025-03-09 15:05:15 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,17 @@
+import jwt from "jsonwebtoken";
 
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) return res.status(403).json({ message: "Access Denied" });
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+export default authMiddleware;
