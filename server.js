223aad47a8254c1edc9082fbd845680fff4a2b97
mrunali2311 <lily.devil1125@gmail.com>
2025-03-09 15:00:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import seriesRoutes from "./routes/seriesRoutes.js";
 
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/series", seriesRoutes);
+
+// Database Connection
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
